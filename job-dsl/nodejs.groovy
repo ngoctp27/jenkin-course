8d90f2bead66251898363169510079e1f2b59aa5
Ngoc TP <ngoctp27@gmail.com>
2021-01-10 21:07:53 +0700

    fix SCM source

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/ngoctp27/hello-nodejs.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('ngoctp27@gmail.com')
         }
     }
     triggers {
